d40766cc12ae82a9985342fcad31a840b828d343
Nanoarchaeum <2423704835@qq.com>
2026-09-10 23:14:33 +0800

    release: package v0.4.4 build 5

--- test/hard_defects_test.dart
+++ test/hard_defects_test.dart
@@ -182,7 +182,7 @@ void main() {
       );
       expect(
         File('pubspec.yaml').readAsStringSync(),
-        contains('version: 0.4.4+4'),
+        contains('version: 0.4.4+5'),
       );
       final nsi = File('install.nsi').readAsStringSync();
       expect(nsi, contains('DisplayVersion" "0.4.4"'));
