1618e5352176611066de4157b04c586e4291ac2c
Nanoarchaeum <2423704835@qq.com>
2026-09-11 00:32:49 +0800

    release: publish SyphonNov v5.0.0

--- test/hard_defects_test.dart
+++ test/hard_defects_test.dart
@@ -182,10 +182,10 @@ void main() {
       );
       expect(
         File('pubspec.yaml').readAsStringSync(),
-        contains('version: 0.4.4+6'),
+        contains('version: 5.0.0+1'),
       );
       final nsi = File('install.nsi').readAsStringSync();
-      expect(nsi, contains('DisplayVersion" "0.4.4"'));
+      expect(nsi, contains('DisplayVersion" "5.0.0"'));
       expect(nsi, isNot(contains('SyphonNov2')));
       expect(File('.github/workflows/windows.yml').existsSync(), isTrue);
     },
