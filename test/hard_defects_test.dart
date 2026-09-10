3364ffebddb17892ae3df878615cc83b5c54ea16
Nanoarchaeum <2423704835@qq.com>
2026-09-10 14:23:43 +0800

    release: publish v0.4.3 interaction patch

--- test/hard_defects_test.dart
+++ test/hard_defects_test.dart
@@ -182,7 +182,7 @@ void main() {
       );
       expect(
         File('pubspec.yaml').readAsStringSync(),
-        contains('version: 0.4.3+1'),
+        contains('version: 0.4.3+2'),
       );
       final nsi = File('install.nsi').readAsStringSync();
       expect(nsi, contains('DisplayVersion" "0.4.3"'));
